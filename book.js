a0e7faaf19be1960ec982689f0404a3e939d2c3a
arjumand12 <50356508+arjumand12@users.noreply.github.com>
2019-05-09 14:19:21 +0500

    Update book.js

--- book.js
+++ book.js
@@ -1,12 +1,14 @@
 
-var author = "Syed Arjumand Shehzad";
-var category = "Novel";
-var yearPublished = 1997;
-var numberOfCopiesSold = 20000;
-var numberofPages = 450;
-var price = 2000;
+var author = "Syed Arjumand Shehzad";   // Author Name (Strings)
+var category = "Novel";                 // Book Name (Strings)
+var yearPublished = 1997;               // Published Year (number)
+var numberOfCopiesSold = 20000;         // Number of Copies (number)
+var numberofPages = 450;                // Number of Pages (number)
+var price = 2000;                       // Book Price (number)
+var edition = ["2002", "2003", "2004"]; // Edition (Array)
+edition[2] = "2016"                     // Changing an Array Element
 
-var Condition = numberofPages > 400 ? true : false;
+var Condition = numberofPages > 400 ? true : false; //Condition for number of Pages
 
 var book = {
     Author : author,
@@ -15,8 +17,37 @@ var book = {
     numberOfCopiesSold : numberOfCopiesSold,
     numberofPages : numberofPages,
     Price : price,
-    Condition : Condition
+    Condition : Condition,
+    Edition : edition[2]
 }
 console.log("_______________");
 console.log(book);
+console.log("_______________");
+
+console.log("_______________");
+console.log(author);
+console.log("_______________");
+
+console.log("_______________");
+console.log(category);
+console.log("_______________");
+
+console.log("_______________");
+console.log(yearPublished);
+console.log("_______________");
+
+console.log("_______________");
+console.log(numberOfCopiesSold);
+console.log("_______________");
+
+console.log("_______________");
+console.log(numberofPages);
+console.log("_______________");
+
+console.log("_______________");
+console.log(price);
+console.log("_______________");
+
+console.log("_______________");
+console.log(edition[2]);
 console.log("_______________");
